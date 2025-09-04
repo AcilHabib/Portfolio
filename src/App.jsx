c7c4af0bbf8056e627f84cddc14a58ba1cd59a5c
Assil Habib <assilehabib@gmail.com>
2025-09-04 18:49:43 +0100

    fix deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import AboutProvider from './contexts/AboutContext'
